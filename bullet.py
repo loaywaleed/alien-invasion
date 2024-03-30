9d8c5623ae37930505b64f2f4ac0fa80f110e097
loaywaleed <loaywaleed9@gmail.com>
2024-03-30 19:35:21 +0200

    settings documentation

--- bullet.py
+++ bullet.py
@@ -11,16 +11,12 @@ class Bullet(Sprite):
     def __init__(self, game_settings):
         """Instantiation of a new bullet"""
         super().__init__()
-
         self.window = game_settings.window
         self.settings = game_settings.settings
         self.color = self.settings.bullet_color
-
         self.rect = pygame.Rect(
             0, 0, self.settings.bullet_width, self.settings.bullet_height)
-
         self.rect.midtop = game_settings.ship.rect.midtop
-
         self.y = float(self.rect.y)
 
     def update(self):
--- settings.py
+++ settings.py
@@ -1,10 +1,18 @@
+"""
+Module that includes game settings
+"""
+
+
 class Settings:
+    """Game's setting representation"""
+
     def __init__(self):
+        """Instantiation of games setting"""
+        # Screen settings.
         self.screen = (1200, 800)
         self.bg_color = (0, 0, 0)
-
         # Bullet settings.
         self.bullet_width = 3
         self.bullet_speed = 1
-        self.bullet_height = 15
-        self.bullet_color = (180, 180, 180)
+        self.bullet_height = 17
+        self.bullet_color = (180, 0, 0)
