04c324c73c865cc2de855f0f4aaac972dfd3b9cc
loaywaleed <loaywaleed9@gmail.com>
2024-03-30 19:28:46 +0200

    ship module documentation

--- ship.py
+++ ship.py
@@ -1,16 +1,21 @@
+"""
+Module that has ship representation
+"""
 import pygame
 
 
 class Ship:
+    """Space ship representation"""
+
     def __init__(self, game_settings):
+        """Instantiation of the ship"""
         self.window = game_settings.window
         self.screen_rect = game_settings.window.get_rect()
-
         # loading ship image
         self.image = pygame.image.load("imgs/falcon.bmp")
         self.rect = self.image.get_rect()
-
         self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
+        """Drawing the ship"""
         self.window.blit(self.image, self.rect)
