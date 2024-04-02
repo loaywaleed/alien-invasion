aee3b3b4dad720c718044f7d3ef4c0c5d41e1602
loaywaleed <loaywaleed9@gmail.com>
2024-04-02 15:37:44 +0200

    documenting alien class

--- alien.py
+++ alien.py
@@ -7,7 +7,10 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
+    """Class which represents and alien"""
+
     def __init__(self, game_settings):
+        """Instantation of alien"""
         super().__init__()
         self.settings = game_settings.settings
         self.window = game_settings.window
@@ -21,6 +24,6 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def update(self):
-        """Updates position of alien"""
+        """Updates position of alien and makes it move to the right"""
         self.x += self.settings.alien_speed
         self.rect.x = self.x
