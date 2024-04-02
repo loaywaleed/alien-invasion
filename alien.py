bf29c8f6fbf72d75b283a3ebadc794ed47f2a2dc
loaywaleed <loaywaleed9@gmail.com>
2024-04-02 16:15:51 +0200

    documentation typos

--- alien.py
+++ alien.py
@@ -7,10 +7,10 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
-    """Class which represents and alien"""
+    """Class which represents an alien"""
 
     def __init__(self, game_settings):
-        """Instantation of alien"""
+        """Instantiation of alien"""
         super().__init__()
         self.settings = game_settings.settings
         self.window = game_settings.window
@@ -24,7 +24,7 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def update(self):
-        """Updates position of alien and makes it move to the right"""
+        """Updates position of the alien and makes it move to the right"""
         self.x += self.settings.alien_speed
         self.rect.x = self.x
 
