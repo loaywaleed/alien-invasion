0278751b308ea4a133b3306d75c966cb4691d80a
loaywaleed <loaywaleed9@gmail.com>
2024-11-22 13:10:23 +0200

    resetting stats after play button shows

--- alien-invasion.py
+++ alien-invasion.py
@@ -179,8 +179,14 @@ class GameSettings:
                 break
 
     def _check_play_button(self, mouse_pos):
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            self.stats.reset_stats()
             self.stats.game_active = True
+            self.aliens.empty()
+            self.bullets.empty()
+            self._create_aliens()
+            self.ship.center_ship()
 
 
 if __name__ == '__main__':
